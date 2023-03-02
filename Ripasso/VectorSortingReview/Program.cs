0ce15cc247f48dcd067e19cdb81dd5343543fa98
Leonardo Frassineti <126104442+HattyDoge@users.noreply.github.com>
2023-03-02 09:49:50 +0100

    Selection Sort
    Aggiunta del selection Sort

---
+++ Ripasso/VectorSortingReview/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace VectorSortingReview
+{
+    internal class Program
+    {
+        static void Swap(ref int value1, ref int value2)
+        {
+            int temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
+        static void SelectionSort(int[] vector)
+        {
+            for(int i = 0; i < vector.Length; i++)
+            {
+                int jMin = i;
+                for (int j = 0; j < vector.Length; j++)
+                    if (vector[jMin] > vector[j])
+                        jMin = j;
+                Swap(ref vector[i], ref vector[jMin]);
+            }
+        }
+        static void BubbleSort(int[] vector)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- WPFApp/PrimoEsercizioWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WPFApp/PrimoEsercizioWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -405,7 +405,7 @@ namespace PrimoEsercizioWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PrimoEsercizioWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PrimoEsercizioWPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
