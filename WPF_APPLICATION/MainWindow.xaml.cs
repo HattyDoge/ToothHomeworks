5a000d3152349ae8ecaab4e70965c66c6e70b0c2
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-03-16 10:41:47 +0100

    Need to Develop

--- WPF_APPLICATION/MainWindow.xaml.cs
+++ WPF_APPLICATION/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,10 +27,19 @@ namespace WPF_APPLICATION
         public int age;
         public int height;
         public int weight;
+        public int activity;
     }
-    
+    public enum attività
+    {
+        Commerciante = 0,
+        Dipendente,
+        LiberoProfessionista,
+        Artigiano,
+        Imprenditore
+    }
     public partial class MainWindow : Window
     {
+        public int attivitàScelta;
         public MainWindow()
         {
             InitializeComponent();
@@ -39,29 +49,29 @@ namespace WPF_APPLICATION
         {
             peopleIndex = 0;
             People[] people = new People[50];
-            people[peopleIndex].surname = "Pippo01"; people[peopleIndex].name = "Pippo02"; people[peopleIndex].weight = 80; people[peopleIndex].height = 190; people[peopleIndex].age = 46;
+            people[peopleIndex].surname = "Pippo01"; people[peopleIndex].name = "Pippo02"; people[peopleIndex].weight = 80; people[peopleIndex].height = 190; people[peopleIndex].age = 46; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo11"; people[peopleIndex].name = "Pippo12"; people[peopleIndex].weight = 50; people[peopleIndex].height = 160; people[peopleIndex].age = 35;
+            people[peopleIndex].surname = "Pippo11"; people[peopleIndex].name = "Pippo12"; people[peopleIndex].weight = 50; people[peopleIndex].height = 160; people[peopleIndex].age = 35; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo21"; people[peopleIndex].name = "Pippo22"; people[peopleIndex].weight = 100; people[peopleIndex].height = 190; people[peopleIndex].age = 60;
+            people[peopleIndex].surname = "Pippo21"; people[peopleIndex].name = "Pippo22"; people[peopleIndex].weight = 100; people[peopleIndex].height = 190; people[peopleIndex].age = 60; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo31"; people[peopleIndex].name = "Pippo32"; people[peopleIndex].weight = 76; people[peopleIndex].height = 187; people[peopleIndex].age = 25;
+            people[peopleIndex].surname = "Pippo31"; people[peopleIndex].name = "Pippo32"; people[peopleIndex].weight = 76; people[peopleIndex].height = 187; people[peopleIndex].age = 25; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo41"; people[peopleIndex].name = "Pippo42"; people[peopleIndex].weight = 50; people[peopleIndex].height = 160; people[peopleIndex].age = 32;
+            people[peopleIndex].surname = "Pippo41"; people[peopleIndex].name = "Pippo42"; people[peopleIndex].weight = 50; people[peopleIndex].height = 160; people[peopleIndex].age = 32; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo51"; people[peopleIndex].name = "Pippo52"; people[peopleIndex].weight = 78; people[peopleIndex].height = 175; people[peopleIndex].age = 25;
+            people[peopleIndex].surname = "Pippo51"; people[peopleIndex].name = "Pippo52"; people[peopleIndex].weight = 78; people[peopleIndex].height = 175; people[peopleIndex].age = 25; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo61"; people[peopleIndex].name = "Pippo62"; people[peopleIndex].weight = 95; people[peopleIndex].height = 190; people[peopleIndex].age = 38;
+            people[peopleIndex].surname = "Pippo61"; people[peopleIndex].name = "Pippo62"; people[peopleIndex].weight = 95; people[peopleIndex].height = 190; people[peopleIndex].age = 38; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo71"; people[peopleIndex].name = "Pippo72"; people[peopleIndex].weight = 90; people[peopleIndex].height = 180; people[peopleIndex].age = 47;
+            people[peopleIndex].surname = "Pippo71"; people[peopleIndex].name = "Pippo72"; people[peopleIndex].weight = 90; people[peopleIndex].height = 180; people[peopleIndex].age = 47; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo81"; people[peopleIndex].name = "Pippo82"; people[peopleIndex].weight = 100; people[peopleIndex].height = 190; people[peopleIndex].age = 25;
+            people[peopleIndex].surname = "Pippo81"; people[peopleIndex].name = "Pippo82"; people[peopleIndex].weight = 100; people[peopleIndex].height = 190; people[peopleIndex].age = 25; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo91"; people[peopleIndex].name = "Pippo92"; people[peopleIndex].weight = 97; people[peopleIndex].height = 190; people[peopleIndex].age = 42;
+            people[peopleIndex].surname = "Pippo91"; people[peopleIndex].name = "Pippo92"; people[peopleIndex].weight = 97; people[peopleIndex].height = 190; people[peopleIndex].age = 42; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo101"; people[peopleIndex].name = "Pippo102"; people[peopleIndex].weight = 84; people[peopleIndex].height = 168; people[peopleIndex].age = 25;
+            people[peopleIndex].surname = "Pippo101"; people[peopleIndex].name = "Pippo102"; people[peopleIndex].weight = 84; people[peopleIndex].height = 168; people[peopleIndex].age = 25; people[peopleIndex].activity = 0;
             peopleIndex++;
-            people[peopleIndex].surname = "Pippo111"; people[peopleIndex].name = "Pippo112"; people[peopleIndex].weight = 90; people[peopleIndex].height = 175; people[peopleIndex].age = 58;
+            people[peopleIndex].surname = "Pippo111"; people[peopleIndex].name = "Pippo112"; people[peopleIndex].weight = 90; people[peopleIndex].height = 175; people[peopleIndex].age = 58; people[peopleIndex].activity = 0;
             string fileName = "C:\\Users\\leonardo.frassineti\\Desktop\\MolareToothHomeworks\\WPF_APPLICATION\\DataFile.txt";
             using (StreamWriter sw = new StreamWriter(fileName))
             {
@@ -71,13 +81,52 @@ namespace WPF_APPLICATION
                 }
                 sw.Close();
             }
-            Vbx_InputData.Visibility = Visibility.Visible;
             Btn_InsertNewData.Visibility = Visibility.Visible;
         }
+        public void UncheckAll()
+        {
+            RBCommerciante_0.IsChecked = false;
+            RBDipendente_1.IsChecked = false;
+            RBLiberoProfessionista_2.IsChecked = false;
+            RBArtigiano_3.IsChecked = false;
+            RBImprenditore_4.IsChecked = false;
+        }
+        private void Attività_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = (RadioButton)sender;
 
+            switch (radioButton.Name)
+            {
+                case "RBCommerciante_0":
+                    attivitàScelta = (int)attività.Commerciante;
+                    break;
+                case "RBDipendente_1":
+                    attivitàScelta = (int)attività.Dipendente;
+                    break;
+                case "RBLiberoProfessionista_2":
+                    attivitàScelta = (int)attività.LiberoProfessionista;
+                    break;
+                case "RBArtigiano_3":
+                    attivitàScelta = (int)attività.Artigiano;
+                    break;
+                case "RBImprenditore_4":
+                    attivitàScelta = (int)attività.Imprenditore;
+                    break;
+            }
+        }
         private void Btn_InsertNewData_Click(object sender, RoutedEventArgs e)
         {
-            
+            Vbx_InputData.Visibility = Visibility.Visible;
+            GpBx_Attività.Visibility = Visibility.Visible;
+        }
+        private void Btn_Order_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_File_Output_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
