5fe242b041437ff80d3ea7cee23a65439bd6ba78
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-03-24 00:18:51 +0100

    Help me

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace List
+﻿using System.Runtime.CompilerServices;
+
+namespace List
 {
 	internal class Program
 	{
@@ -12,11 +14,28 @@
 		}
 		static void Main(string[] args)
 		{
+			string fileName = "..\\..\\Datafile";
+			List<Product> productsShop = new List<Product>();
 			using (StreamReader sr = new StreamReader(fileName))
 			{
 				while (!sr.EndOfStream)
 				{
+					string[] linea = sr.ReadLine().Split("|");
+					for (int i = 0; i < linea.Length; i++)
+					{
+						Product p = new Product();
+						p.code = int.Parse(linea[i]);
+						p.name = linea[i];
+						p.producer = linea[i];
+						p.type = linea[i];
+						p.price = double.Parse(linea[i]);
+						
+						productsShop.Add(p);
+					}
 				}
+				sr.Close();
 			}
+
+		}
 	}
 }
