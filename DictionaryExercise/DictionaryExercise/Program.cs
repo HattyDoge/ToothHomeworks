784270f2c9e37cf43eca1313b4840bfe3a9e9ffd
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-03-28 22:57:48 +0200

    Finished Project
    Actually finished

--- DictionaryExercise/DictionaryExercise/Program.cs
+++ DictionaryExercise/DictionaryExercise/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
 namespace DictionaryExercise
 {
 	internal class Program
 	{
+		static string[] Input(out double value1, out double value2, Dictionary<string, double> universalConstraint)
+		{
+			string[] operation = { "2", "3", "4" };
+			while (true)
+			{
+				operation = Console.ReadLine().Split(" ");
+				if (!universalConstraint.TryGetValue(operation[0], out value1))
+					if (!double.TryParse(operation[0], out value1))
+					{
+						Console.WriteLine("Primo valore non corretto\nReinserire i valori");
+						continue;
+					}
+				if (!universalConstraint.TryGetValue(operation[2], out value2))
+					if (!double.TryParse(operation[2], out value2))
+					{
+						Console.WriteLine("Secondo valore non corretto\nReinserire i valori");
+						continue;
+					}
+				break;
+			}
+
+			return operation;
+		}
 		static void Main(string[] args)
 		{
 			Dictionary<string, double> universalConstraint = new Dictionary<string, double>();
-			
+
 			// Add some elements to the dictionary. There are no
 			// duplicate keys, but some of the values are duplicates.
 			universalConstraint.Add("e", 2.718281828459045);
@@ -17,25 +42,41 @@ namespace DictionaryExercise
 			universalConstraint.Add("c", 300000);
 
 			Console.WriteLine("Fai un operazioni tra constanti o con numeri : valore operatore valore");
+			string[] operation;
+			double value1;
+			double value2;
+			double result = 0;
 			while (true)
 			{
-				string[] operation = Console.ReadLine().Split(" ");
-				double value1;
-				double value2;
-				if (!universalConstraint.TryGetValue(operation[0], out value1))
-					if (!double.TryParse(operation[0], out value1))
-					{
-						Console.WriteLine("Primo valore non corretto\nReinserire i valori");
-						continue;
-					}
-				if (!universalConstraint.TryGetValue(operation[0], out value2))
-					if (!double.TryParse(operation[0], out value2))
-					{
-						Console.WriteLine("Secondo valore non corretto\nReinserire i valori");
-						continue;
-					}
+				operation = Input(out value1, out value2, universalConstraint);
+				bool switchHappened = false;
+				switch (operation[1])
+				{
+					case "+":
+						switchHappened = true;
+						result = value1 + value2;
+						break;
 
+					case "-":
+						switchHappened = true;
+						result = value1 - value2;
+						break;
+
+					case "*":
+						switchHappened = true;
+						result = value1 * value2;
+						break;
+
+					case "/":
+						switchHappened = true;
+						result = value1 / value2;
+						break;
+				}
+				if (!switchHappened)
+					continue;
+				break;
 			}
+			Console.WriteLine($"Il risultato dell'operazione tra {operation[0]} {operation[1]} {operation[2]} è {value1} {operation[1]} {value2} = {result} ");
 		}
 	}
 }
