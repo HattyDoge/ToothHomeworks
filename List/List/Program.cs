6fa4fc2f295a6df14689d01da9b2ae780ee81943
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-04-13 08:34:16 +0200

    Added List Functions
    LOL I FORGOT TO PUSH THE LAST LIST PROGRAM LOLOLOLOL

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace List
+﻿namespace List
 {
 	internal class Program
 	{
@@ -11,6 +9,53 @@ namespace List
 			public string producer;
 			public string type;
 			public double price;
+		}
+		static void Read(List<Product> productsShop)
+		{
+            string[] linea = Console.ReadLine().Split("|");
+            for (int i = 0; i < linea.Length; i++)
+            {
+                Product p = new Product();
+                p.code = int.Parse(linea[i]);
+                p.name = linea[i];
+                p.producer = linea[i];
+                p.type = linea[i];
+                p.price = double.Parse(linea[i]);
+
+                productsShop.Add(p);
+            }
+        }
+		static void ProductWrite(List<Product> productsShop)
+		{
+			Console.WriteLine("Inserire un produttore");
+			string producer = Console.ReadLine();
+			int producerProductCount = 0;
+			for(int i = 0; i < productsShop.Count; i++)
+			{
+				if (productsShop[i].producer == producer)
+					producerProductCount++;
+			}
+			Console.WriteLine($"Il numero dei prodotti che {producer} ha sono {producerProductCount}");
+		}
+		static void NameWrite()
+		{
+
+		}
+		static void CodeWrite()
+		{
+
+		}
+		static void TypeWrite()
+		{
+
+		}
+		static void WriteInFile()
+		{
+
+		}
+		static void ReadFromFile() 
+		{
+			
 		}
 		static void Main(string[] args)
 		{
@@ -35,6 +80,7 @@ namespace List
 				}
 				sr.Close();
 			}
+			
 
 		}
 	}
