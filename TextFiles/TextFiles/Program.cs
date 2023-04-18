58a00f3338ad9e4ea229c2d2c4844956ff816f0e
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-04-18 18:37:23 +0200

    Added solution
    New project about StreamReader

---
+++ TextFiles/TextFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TextFiles
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const string fileRep = @"..\..\..\TextFile.txt";
+			int[] vector = new int[100];
+			try
+			{
+				using (StreamReader sr = new StreamReader(fileRep))
+				{
+					while (!sr.EndOfStream)
+					{
+						
+						try
+						{
+							string[] line = sr.ReadLine().Split(" ");
+							int[] numbers = { int.Parse(line[0]), int.Parse(line[1]) };
+							if (numbers[0] > 99 || numbers[0] < 0)
+								numbers[0] = 0;
+							vector[numbers[0]] = numbers[1];
+						}
+						catch
+						{
+							Console.WriteLine("The vector can't be acquired");
+						}
+					}
+				}
+
+			} 
+			catch (FileNotFoundException) 
+			{
+				Console.WriteLine("File in repository missing");
+			}
+			for (int i = 0; i < vector.Length; i++)
+			{
+				Console.WriteLine($"{i} {vector[i]}");
+			}
+		}
+	}
+}
