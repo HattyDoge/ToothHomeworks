8dca13be92541573cbc8348a53343b8fb1d7f0a1
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-04-19 23:55:22 +0200

    fixed sort
    wtf sort!

--- WPF_APPLICATION/MainWindow.xaml.cs
+++ WPF_APPLICATION/MainWindow.xaml.cs
@@ -189,7 +189,7 @@ namespace WPF_APPLICATION
         }
         private bool CompareString(string str1, string str2)
         {
-            if(str1.Length < str2.Length)
+            if(str1.Length > str2.Length)
             {
                 string temp = str1;
                 str1 = str2;
@@ -197,8 +197,11 @@ namespace WPF_APPLICATION
             }    
             for(int i = 0; i < str1.Length; i++)
             {
-                if (str1[i] < str2[i])
+				if (str1[i] < str2[i])
+					return false;
+				if (str1[i] > str2[i])
                     return true;
+
             }
             return false;
         }
