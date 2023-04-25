8b4e01b7981bcfb54ee45246e7c397b03d389c55
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-04-25 17:03:57 +0200

    Created TunaAndShark
    Simulation of a lake with tunas and sharks

---
+++ TunaAndSharks/TunaAndSharks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TunaAndSharks
+{
+	internal class Program
+	{
+		enum Type
+		{
+			shark,
+			tuna
+		}
+		class Shark { }
+		class Tuna { }
+		
+		class Cell
+		{
+			public Type type;
+			public Shark shark;
+			public Tuna tuna;
+		}
+		static void
+		
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
