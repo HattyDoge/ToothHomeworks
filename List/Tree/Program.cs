685df552966bee42358f75715e300abd84a99485
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-04-25 22:10:20 +0200

    Added list and tree project

---
+++ List/Tree/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace TreesAndLists
+{
+	internal class Program
+	{
+		class Node
+		{
+			public int value;  // valore memorizzato
+			public Node succ;    // riferimento al valore (nodo) successivo
+		}
+		class List
+		{
+			public Node testa;  // riferimento al primo elemento della lista
+		}
+		static int CountValuesList(List l)
+		{
+			Node nodesucc = l.testa;
+			int count = 0;
+			while (nodesucc.succ != null)
+			{
+				if (nodesucc.value != null)
+					count++;
+				nodesucc = nodesucc.succ;
+			}
+			return count;
+		}
+		static void CountValuesTree()
+		{
+
+		}
+		static void DeleteValueList(List l, int value)
+		{
+			for (ref Node nodo = ref l.testa; nodo != null; nodo = ref nodo.succ)
+			{
+				if (nodo.value == value)
+				{
+					nodo = nodo.succ; 
+				}
+			}
+		}
+		static void NewNode(List l, int valore)
+		{
+			ref Node nodo = ref l.testa;
+			while (nodo != null)
+				nodo = ref nodo.succ;
+			nodo = new Node();
+			nodo.value = valore;
+		}
+		static void Main(string[] args)
+		{
+			List l = new List();
+			NewNode(l, 1);
+			NewNode(l, 10);
+			NewNode(l, 1);
+			NewNode(l, 1);
+			NewNode(l, 10);
+			NewNode(l, 1);
+			NewNode(l, 2);
+			DeleteValueList(l, 10);
+		}
+	}
+}
