b1d5b5d7cee1c00bb780734fff94fdd35212f2fe
BananaDoge <126104442+HattyDoge@users.noreply.github.com>
2023-09-15 12:57:56 +0200

    new project

---
+++ ProvaConsoleCS/ProvaConsoleCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ProvaConsoleCS
+{
+    internal class Program
+    {
+        static bool Check(string ex) 
+        {
+            List<char> stack = new List<char>();
+            foreach ( char s in ex) 
+            {
+                if (s == '(' || s == '{' || s == '[') ;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            //  controllo bilanciamento delle parentesi 
+
+        }
+    }
+}
