8ef25dcca2181efd6506791ceb5e59c4db6c9e1f
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-09-19 16:33:50 +0200

    new project with a finished function
    deleted it and readded it

---
+++ ConsoleProva/ConsoleProva/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace ProvaConsoleCS
+{
+	internal class Program
+	{
+		static bool Check(string ex)
+		{
+			List<char> stack = new List<char>();
+			foreach (char s in ex)
+			{
+				if (s == '(')
+					stack.Add(')');
+				if( s == '{')
+					stack.Add('}');
+				if( s == '[')
+					stack.Add(']');
+
+				if (s == ')' || s == '}' || s == ']')
+				{
+					if (s == stack[stack.Count - 1])
+					{
+						stack.RemoveAt(stack.Count - 1);
+					}
+					else
+						return false;
+				}
+			}
+			if (stack.Count > 0)
+				return false;
+			return true;
+
+		}
+		static void Main(string[] args)
+		{
+			//  controllo bilanciamento delle parentesi 
+			string exp = "{( 5 + 5 )[ 23 * {32 + 3} - 34 (23 + 3) ]}";
+			if (Check(exp))
+				Console.WriteLine("Giusto");
+			else
+				Console.WriteLine("Sbagliato");
+
+			exp = "{( 5 + 5 ){[ 23 * {32 + 3} - 34 (23 + 3) ]}";
+			if (Check(exp))
+				Console.WriteLine("Giusto");
+			else
+				Console.WriteLine("Sbagliato");
+
+			exp = "{( 5 + 5 ){[ 23 * {32 + 3} - 34 (23 + 3)] ]}}";
+			if (Check(exp))
+				Console.WriteLine("Giusto");
+			else
+				Console.WriteLine("Sbagliato");
+		}
+	}
+}
