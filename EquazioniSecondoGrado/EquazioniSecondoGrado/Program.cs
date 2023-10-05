d463d4c97fda594745770fbe1368bf35fd4c53c6
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-10-05 08:58:11 +0200

    added two new projects
    one working the other one no

---
+++ EquazioniSecondoGrado/EquazioniSecondoGrado/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EquazioniSecondoGrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 2;
+            double c = -3;
+            double delta = b * b - 4.0 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+
+            double Q;       // metodo precisio
+            if (b >= 0 ) 
+                Q = (b + Math.Sqrt(delta)) / 2;
+            else
+                Q = (b - Math.Sqrt(delta)) / 2;
+            x1 = Q / a;     // metodo precisio
+            x2 = c / Q;     // metodo precisio
+            Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+        }
+    }
+}
---
+++ ExercisesTooth2023-2024/ExerciseClassesProprieties/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ExerciseClassesProprieties
+{
+    enum Subjects
+    {
+        Matematica,
+        SistemiReti,
+        Informatica,
+        Inglese,
+        Letteratura,
+        TPSIT,
+        Telecomunicazioni
+    }
+    class SchoolClass
+    {
+        int subjectsNum = 7;
+        struct Scholar
+        {
+            public int registerNumber;
+            public DateOnly birthDate;
+            public double[] averageGrade;
+            public string name;
+            public string surname;
+        }
+
+        SchoolClass(int classSize)
+        {
+            Scholar[] scholar = new Scholar[classSize];
+            for(int i = 0; i< scholar.Length; i++)
+                scholar[i].averageGrade = new double[subjectsNum];
+        }
+        public Scholar GetStudentAt()
+        {
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
