b6711600e082c0178dbe0e6eed92054b4eac34b2
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-10-19 08:58:35 +0200

    added stuff

--- ExercisesTooth2023-2024/Operators/Program.cs
+++ ExercisesTooth2023-2024/Operators/Program.cs
@@ -1,6 +1,8 @@
-﻿namespace Operators
+﻿using System.Drawing;
+
+namespace Operators
 {
-    class Rational : Object
+    class Rational : Object, IComparable<Rational>
     {
         public override string ToString()
         {
@@ -29,11 +31,11 @@
         } */
         public static Rational operator + (Rational r1, Rational r2) // Operator è molto intuitivo
         {
-            return new Rational(r1.numerator * r1.denominator + r2.numerator * r2.denominator, r1.denominator * r2.denominator);
+            return new Rational(r1.numerator * r2.denominator + r2.numerator * r1.denominator, r1.denominator * r2.denominator);
         }
         public static Rational operator - (Rational r1, Rational r2)
         {
-            return new Rational(r1.numerator * r1.denominator - r2.numerator * r2.denominator, r1.denominator * r2.denominator);
+            return new Rational(r1.numerator * r2.denominator - r2.numerator * r1.denominator, r1.denominator * r2.denominator);
         }
         public static Rational operator * (Rational r1, Rational r2)
         {
@@ -47,6 +49,24 @@
         {
             return (double)r.numerator / r.denominator;
         }
+        public override bool Equals(Object? obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is Rational)) // is as
+                return false;
+            return this == (Rational)obj;
+        }
+        public int CompareTo(Rational? other)
+        {
+            return numerator * other.numerator - numerator * other.denominator;
+        }
+        public static bool operator < (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) < 0; }
+        public static bool operator <= (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) <= 0; }
+        public static bool operator == (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) == 0; }
+        public static bool operator != (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) != 0; }
+        public static bool operator > (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) > 0; }
+        public static bool operator >= (Rational r_sx, Rational r_dx) { return r_sx.CompareTo(r_dx) >= 0; }
         void Normalize()
         {
             if (this.denominator < 0)   //  Normalizzazione
@@ -77,11 +97,25 @@
         {
             Rational A = new Rational(2, 4);
             Rational B = new Rational(2, 5);
-      //    Rational sum = Rational.Sum(A, B);
-      //    Console.WriteLine(sum);
+            Console.WriteLine(A);
+            //    Rational sum = Rational.Sum(A, B);
+            //    Console.WriteLine(sum);
             Rational sum = A + B;
             Console.WriteLine(sum);
             Console.WriteLine((double)sum); // (double) <-- explicit ( lo può fare anche implicit )
+            Console.WriteLine($"Il valore più piccolo fra {A} e {B} {(A < B ? A : B)}");  // ( in parentesi perchè sennò la interpreterebbe come un modo di trascrivere i valori)
+
+            List<Rational> l = new List<Rational>();
+            foreach (Rational r in l)
+            {
+                Console.Write($"{r}, ");
+            }
+            Console.WriteLine();
+            l.Sort();
+            foreach (Rational r in l)
+            {
+                Console.Write($"{r}, ");
+            }
         }
     }
 }
