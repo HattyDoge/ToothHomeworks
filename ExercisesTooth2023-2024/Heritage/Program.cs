fb7e4b70778012d0751d8de369f775df49568829
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-10-26 08:50:12 +0200

    I'm sad

--- ExercisesTooth2023-2024/Heritage/Program.cs
+++ ExercisesTooth2023-2024/Heritage/Program.cs
@@ -5,6 +5,7 @@
         class A
         {
             int a;
+            public int defineA { get { return a; } set { a = value; } }
             public A(int a) 
             {
                 this.a = a; 
@@ -13,6 +14,7 @@
         class B : A
         {
             int b;
+            public int defineB { get { return b; } set { b = value; } }
             public B(int b ,int a) : base(a)
             {
                 this.b = b;
@@ -21,6 +23,7 @@
         class E : B 
         {
             int e;
+            public int defineE { get { return e; } set { e = value; } }
             public E(int e, int b, int a) : base(b, a)
             {
                 this.e = e;
@@ -29,6 +32,7 @@
         class F : B
         {
             public int f;
+            public int defineF { get { return f; } set { f = value; } }
             public F(int f, int b, int a) : base(b, a)
             {
                 this.f = f;
@@ -37,6 +41,7 @@
         class C : A
         {
             public int c;
+            public int defineC { get { return c; } set { c = value; } }
             public C(int c, int a) : base(a)
             {
                 this.c = c;
@@ -45,6 +50,7 @@
         class D : A
         {
             int d;
+            public int defineD { get { return d; } set { d = value; } }
             public D(int d, int a) : base(a)
             {
                 this.d = d;
@@ -53,14 +59,25 @@
         class G : D
         {
             int g;
+            public int defineG { get { return g; } set { g = value; } }
             public G(int g,int d, int a) : base(d, a)
             {
                 this.g = g;
             }
         }
+        class H : D
+        {
+            int h;
+            public int defineH { get { return h; } set { h = value; } }
+            public H(int h, int d, int a) : base(d, a)
+            {
+                this.h = h;
+            }
+        }
         class I : G
         { 
             int i;
+            public int defineI { get { return i; } set { i = value; } }
             public I(int i, int g, int d, int a) : base(g, d, a)
             {
                 this.i = i;
@@ -69,17 +86,10 @@
         class L : G
         {
             public int l;
+            public int defineL { get { return l; } set { l = value; } }
             public L(int l, int g, int d, int a) : base(g, d, a)
             {
                 this.l = l;
-            }
-        }
-        class H : D
-        {
-            int h;
-            public H(int h, int d, int a) : base(d, a)
-            {
-                this.h = h;
             }
         }
         static void Main(string[] args)
