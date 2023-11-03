c66d1e7156e756460de241f3957f89e47fbd0f04
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-11-03 13:51:08 +0100

    skibidi toilet

--- WPF_Applications/Squadre_Calcio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WPF_Applications/Squadre_Calcio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Squadre_Calcio {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Squadre_Calcio;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Squadre_Calcio;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
