dcc1168d26f4341c96838b2e810ade78541b9a1d
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-11-16 08:58:56 +0100

    I'm sleepy

---
+++ ExercisesTooth2023-2024/GeneretorFunctions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GeneretorFunctions
+{
+    internal class Program
+    {
+        interface IComeTiVoglioVedere
+        {
+            void CompitoImportantissimo();
+        }
+
+        class Base_A : IComeTiVoglioVedere
+        {
+            void IComeTiVoglioVedere.CompitoImportantissimo() { }
+        }
+        class Derivata_A : Base_A
+        {
+
+        }
+        class Base_B
+        {
+
+        }
+        class Derivata_B : Base_B
+        { 
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
