8ce77b47bde841c0612fd17c1375fd1b78fb9624
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-12-04 13:04:04 +0100

    SHUSH

---
+++ ExercisesTooth2023-2024/ExpressionEvaluator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExpressionEvaluator
+{
+    enum TokenTypes
+    {
+        ConstValue,
+        LBra,       // (
+        RBra,       // )
+        OpPlus,     // +
+        OpMinus,    // -
+        OpMult,     // *
+        OpDiv,      // /
+        OpMod       // %
+    }
+    class Token
+    {
+        public TokenTypes Type { get; private set; }
+        public double Value { get; private set; }   // valido se .Type == ConstValue
+        public Token(TokenTypes type, double value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
